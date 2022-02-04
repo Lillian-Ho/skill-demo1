16a5120b76ce5fe7e52f57e3885263fe54ac3bba
LillianHo5 <lmho@ucsd.edu>
2022-02-03 20:01:15 -0800

    Update SkillDemoTest.java
    Fix the failing test by changing the expected output value to 6

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -4,6 +4,6 @@ import org.junit.*;
 public class SkillDemoTest {
     @Test
     public void multiply() {
-        assertEquals(25, SkillDemo.multiply(3, 2)); //this test will fail! SkillDemo.multiply(3, 2) will return 6, which does not match the expected 25
+        assertEquals(6, SkillDemo.multiply(3, 2)); //by changing the expected output to 6, the test will now pass since it matches  the output of SkillDemo.multiply(3, 2)
     }
 }
